2705882db667069158bedb2320e82da47f87d111
Omar Faruk <omarfaruk65142@gmail.com>
2024-07-02 00:54:08 +0600

    User delete functionility create

--- src/components/Coffees.jsx
+++ src/components/Coffees.jsx
@@ -1,14 +1,44 @@
-import { Card, Col, Row } from "react-bootstrap";
+import { Button, Card, Col, Row } from "react-bootstrap";
 import View from "../../public/images/view.png"
 
 import Edit from "../../public/images/edit.png"
 import Del from "../../public/images/delete.png"
 import { Link, } from "react-router-dom";
+import Swal from "sweetalert2";
 
 
 const Coffees = ({ coffee }) => {
-    const { name, nameOfChef, supplier, taste, category, details, photo } = coffee
-    console.log(coffee)
+    const { _id, name, nameOfChef, supplier, taste, category, details, photo } = coffee
+
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+
+                fetch(`http://localhost:5000/coffee/${id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your Coffee has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
     return (
 
         <Col >
@@ -27,7 +57,7 @@ const Coffees = ({ coffee }) => {
                     <Col className="d-flex flex-column">
                         <Link to="/edit"><img src={Edit} /></Link>
                         <Link to="/view"><img src={View} /></Link>
-                        <Link to="/delete"><img src={Del} /></Link>
+                        <Link onClick={() => handleDelete(_id)}><img src={Del} /></Link>
                     </Col>
 
                 </Row>
