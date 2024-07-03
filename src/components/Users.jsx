23ffd7db79cb0d3802bafbd3e79a5467996369ba
Omar Faruk <omarfaruk65142@gmail.com>
2024-07-04 00:44:12 +0600

    user delete

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,11 +1,55 @@
 import { useState } from "react";
 import { Container, Table } from "react-bootstrap";
 import { useLoaderData } from "react-router-dom";
-
+import Swal from "sweetalert2";
 
 const Users = () => {
     const loadedUsers = useLoaderData();
-    const [users, setUser] = useState(loadedUsers)
+    const [users, setUsers] = useState(loadedUsers);
+
+    const handleDelete = (_id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/users/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "The user has been deleted.",
+                                icon: "success"
+                            });
+                            const remainingUsers = users.filter(user => user._id !== _id);
+                            setUsers(remainingUsers);
+                        } else {
+                            Swal.fire({
+                                title: "Error!",
+                                text: "User could not be deleted.",
+                                icon: "error"
+                            });
+                        }
+                    })
+                    .catch(error => {
+                        Swal.fire({
+                            title: "Error!",
+                            text: "An error occurred while deleting the user.",
+                            icon: "error"
+                        });
+                    });
+            }
+        });
+    };
+
     return (
         <Container className="mt-5">
             <h2>Total Users {users.length}</h2>
@@ -14,8 +58,9 @@ const Users = () => {
                     <tr>
                         <th>SL</th>
                         <th>Email</th>
-                        <th>Create time</th>
-
+                        <th>Create Time</th>
+                        <th>Last Login</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -24,6 +69,12 @@ const Users = () => {
                             <td>{index + 1}</td>
                             <td>{user.email}</td>
                             <td>{user.createAt}</td>
+                            <td>{user.lastLogin || "N/A"}</td>
+                            <td>
+                                <button onClick={() => handleDelete(user._id)} className="btn btn-danger">
+                                    Delete
+                                </button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
