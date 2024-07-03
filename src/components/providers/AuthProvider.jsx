5a9b82fefa3e0ee160c5546085bfa586c13e003c
Omar Faruk <omarfaruk65142@gmail.com>
2024-07-04 01:22:22 +0600

    Create signin with email and password in authproviders

--- src/components/providers/AuthProvider.jsx
+++ src/components/providers/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { createContext, useState } from "react";
 import app from "../firebase/firebase.config";
 // create app from firebase.config
@@ -7,15 +7,20 @@ const auth = getAuth(app);
 export const AuthContext = createContext(null)
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
-    const [loading,setLoading]=useState(true)
+    const [loading, setLoading] = useState(true)
     const createUser = (email, password) => {
         setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
+    const singInUser = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
     const userInfo = {
         user,
         createUser,
-        loading
+        loading,
+        singInUser
     }
     return (
         <AuthContext.Provider value={userInfo}>
